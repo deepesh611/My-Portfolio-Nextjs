029e77d0f1990551f129451187a6766b6e6ccda2
deepesh611 <deepeshvcd6273@gmail.com>
2024-08-26 10:51:26 +0530

    Updating github link

--- src/components/Floating-Dock.jsx
+++ src/components/Floating-Dock.jsx
@@ -12,7 +12,7 @@ export function SocialDock() {
         {
             title: "GitHub",
             icon: <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-            href: "https://github.com/deepesh611",
+            href: "https://deepesh611.vercel.app/",
         },
         {
             title: "LinkedIn",
