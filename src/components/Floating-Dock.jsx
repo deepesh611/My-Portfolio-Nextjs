739d7ca1e00a631322cca8c9da6f089be38e0a20
deepesh611 <deepeshvcd6273@gmail.com>
2025-02-02 22:27:27 +0530

    Adding Resume

--- src/components/Floating-Dock.jsx
+++ src/components/Floating-Dock.jsx
@@ -7,7 +7,6 @@ import {
     IconBrandLeetcode,
 } from "@tabler/icons-react";
 
-
 export function SocialDock() {
     const links = [
         {
@@ -32,10 +31,41 @@ export function SocialDock() {
         },
     ];
     return (
-        <div className="flex items-center justify-center w-full">
-            <FloatingDock
-                items={links}
-            />
+        <div className="flex flex-col items-center justify-center w-full space-y-4">
+
+        <a href="https://drive.google.com/file/d/1Y4XOwDAMB-F_u0o-BzZ_9BcOPu_2XB4-/view?usp=drive_link" target="_blank">
+            <button
+                className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
+              <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span
+                    className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"/>
+              </span>
+                <div
+                    className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                <span>
+                  Get My Resume
+                </span>
+                    <svg
+                        fill="none"
+                        height="16"
+                        viewBox="0 0 24 24"
+                        width="16"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10.75 8.75L14.25 12L10.75 15.25"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="1.5"
+                        />
+                    </svg>
+                </div>
+                <span
+                    className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"/>
+            </button></a>
+
+            <FloatingDock items={links} />
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,6 +31,7 @@ export function Header() {
                 </div>
 
                 <SocialDock />
+
             </div>
         </header>
   );
